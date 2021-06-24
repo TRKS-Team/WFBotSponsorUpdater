11d440e70d71e446dc247476fe56861c55ad8a43
Cyl18 <cyl18a@gmail.com>
2021-06-25 02:42:41 +0800

    Add project files.

---
+++ Models.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace WFBotSponsorUpdater
+{
+    public partial class ApiResult
+    {
+        public long Ec { get; set; }
+        public string Em { get; set; }
+        public Data Data { get; set; }
+    }
+
+    public partial class Data
+    {
+        public long HasMore { get; set; }
+        [JsonPropertyName("list")]
+        public User[] User { get; set; }
+    }
+
+    public partial class User
+    {
+        public string UserId { get; set; }
+        public long Status { get; set; }
+        public string Name { get; set; }
+        public Uri Avatar { get; set; }
+        public string Cover { get; set; }
+        public string UrlSlug { get; set; }
+        public long IsVerified { get; set; }
+        public long VerifiedType { get; set; }
+        public long IsNotRec { get; set; }
+        public long ShowSponsoring { get; set; }
+        public long HasMark { get; set; }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFBotSponsorUpdater
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sponsors = await GetAllSponsors();
+
+            var sponsorsText = string.Join(' ', sponsors.Select(s => s.Name));
+            const string Magic = "<!--SPONSORS_BEGIN-->";
+            var sponsorsLine = Magic + sponsorsText;
+
+            var lines = await File.ReadAllLinesAsync("README.md", Encoding.UTF8);
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var lineInReadme = lines[i];
+                if (lineInReadme.StartsWith(Magic))
+                {
+                    if (lineInReadme != sponsorsLine)
+                    {
+                        lines[i] = sponsorsLine;
+                    }
+                    break;
+                }
+            }
+            File.WriteAllLines("README.md", lines, Encoding.UTF8);
+        }
+
+        static async Task<List<User>> GetAllSponsors()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36 Edg/91.0.864.54");
+            httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+            int page = 1;
+            ApiResult result;
+            var list = new List<User>();
+            do
+            {
+                result = await httpClient.GetFromJsonAsync<ApiResult>($"https://afdian.net/api/creator/get-sponsors?user_id=6278895e1f1511e9836c52540025c377&type=new&page={page}");
+                if (result!.Ec != 200)
+                {
+                    Console.WriteLine(result);
+                    Environment.Exit(-1);
+                }
+                list.AddRange(result.Data.User);
+                page++;
+
+            } while (result.Data.User.Length != 0);
+
+            return list;
+        }
+    }
+}
